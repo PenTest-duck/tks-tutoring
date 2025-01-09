4168aa2ed68b3c979ebc7079cfc8bd9f2a608494
Chris Yoo <c.yoo@student.unsw.edu.au>
2025-01-09 23:32:23 +1100

    day 4

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -3,16 +3,20 @@ import SheetsTable from "@/components/SheetsTable";
 
 const AdminPage = async () => {
   return (
-    <div className="flex flex-col p-12">
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4 gap-4">
-        <h2 className="text-3xl font-bold">Analytics</h2>
+    <div className="flex flex-col p-12 gap-8">
+      <div className="flex flex-col gap-4">
+        <h2 className="text-3xl font-bold">Overview</h2>
+        <AnalyticsTable />
       </div>
-      <AnalyticsTable />
 
-      <div className="flex flex-col sm:flex-row sm:items-center sm:justify-between mb-4 gap-4">
+      <div className="flex flex-col gap-4">
         <h2 className="text-3xl font-bold">All sheets</h2>
+        <SheetsTable shouldShowAll={true} />
       </div>
-      <SheetsTable shouldShowAll={true} />
+
+      <div className="">
+        <h2 className="text-3xl font-bold">All tutors</h2>
+      </div>
     </div>
   );
 };
